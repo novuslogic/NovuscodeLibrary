a9335e0d63b173b71d9d3d05033701ea2d2ed16e
novuslogic <support@novuslogic.com>
2014-09-18 23:08:06 +1000

    Added NovusForm

---
+++ Source/Core/Forms/frmNovusForm.dfm
@@ -0,0 +1,20 @@
+object hiNovusForm: ThiNovusForm
+  Left = 285
+  Top = 224
+  Caption = 'hiNovusForm'
+  ClientHeight = 380
+  ClientWidth = 456
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  KeyPreview = True
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnKeyPress = FormKeyPress
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ Source/Core/Forms/frmNovusForm.pas
@@ -0,0 +1,91 @@
+unit frmNovusForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, NovusUtilities,StdCtrls ;
+
+type
+  ThiNovusForm = class(TForm)
+    procedure FormShow(Sender: TObject);
+    procedure FormKeyPress(Sender: TObject; var Key: Char);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+    fParentWinControl: TWinControl;
+    fbIgnoreInitWindow: Boolean;
+    fbUseFormKeyPress: Boolean;
+  public
+    { Public declarations }
+    procedure Handle_WM_CLOSEWINDOW(var msg: TMessage); message WM_CLOSEWINDOW;
+
+    function InitWindow: Boolean; virtual;
+
+    procedure SetupWindow; virtual;
+
+    property ParentWinControl: TWinControl
+      read fParentWinControl
+      write fParentWinControl;
+
+    property IgnoreInitWindow: Boolean
+      read fbIgnoreInitWindow
+      write fbIgnoreInitWindow;
+
+    property UseFormKeyPress: Boolean
+      read fbUseFormKeyPress
+      write fbUseFormKeyPress;
+
+  end;
+
+  ThiNovusFormClass = class of ThiNovusForm;
+
+var
+  hiNovusForm: ThiNovusForm;
+
+implementation
+
+
+procedure ThiNovusForm.FormCreate(Sender: TObject);
+begin
+  IgnoreInitWindow := false;
+end;
+
+procedure ThiNovusForm.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+  if UseFormKeyPress = false then Exit;
+
+  if Sender Is TCustomMemo then Exit;
+
+  If Key = #13 Then
+    begin
+      SelectNext(ActiveControl as tWinControl, True, True );
+      Key := #0;
+    end;
+end;
+
+procedure ThiNovusForm.FormShow(Sender: TObject);
+begin
+  if not fbIgnoreInitWindow then InitWindow;
+end;
+
+function ThiNovusForm.InitWindow: Boolean;
+begin
+  Result := True;
+end;
+
+procedure ThiNovusForm.SetupWindow;
+begin
+//
+end;
+
+
+procedure ThiNovusForm.Handle_WM_CLOSEWINDOW(var msg: TMessage);
+begin
+{}
+end;
+
+
+{$R *.dfm}
+
+end.

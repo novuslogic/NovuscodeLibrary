408003e8fb5d15d415ec0389a9e120171ca10c71
support@novuslogic.com <Think098>
2016-10-27 23:10:21 +1100

    Fixed Plugin Sample with Sharemem and Fixed pathing issue in Delphi XE8 Packages

--- Samples/Plugin/Project1.dpr
+++ Samples/Plugin/Project1.dpr
@@ -1,11 +1,11 @@
 program Project1;
 
 uses
+  Sharemem,
   Vcl.Forms,
   Unit1 in 'Unit1.pas' {Form1},
   NovusPlugin in '..\..\Source\Core\Plugin\NovusPlugin.pas',
-  PluginClasses in 'PluginClasses.pas',
-  NovusUtilities in '..\..\Source\Core\Utilities\NovusUtilities.pas';
+  PluginClasses in 'PluginClasses.pas';
 
 {$R *.res}
 
--- Samples/Plugin/TestPlugin.dpr
+++ Samples/Plugin/TestPlugin.dpr
@@ -1,6 +1,7 @@
 library TestPlugin;
 
 uses
+  Sharemem,
   Plugin in 'Plugin.pas',
   NovusPlugin in '..\..\Source\Core\Plugin\NovusPlugin.pas',
   PluginClasses in 'PluginClasses.pas';

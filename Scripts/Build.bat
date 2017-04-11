d33a32a082b831eefcc82a9b3e664730aafc53b9
support@novuslogic.com <Think098>
2017-04-11 23:52:08 +1000

    Update DelphiVersion.inc and Updated all packages to new packages build script

--- Scripts/Build.bat
+++
@@ -1,5 +0,0 @@
-call Zcodegen.exe -project D:\Projects\NovuscodeLibrary\Scripts\packages.zcproject -projectconfig D:\Projects\NovuscodeLibrary\Scripts\packages.zcconfig -var DELPHIVER="DELPHIXE4";LIBSUFFIX="180"
-
-
-
-call pause
--- Scripts/packages.pas
+++ Scripts/packages.pas
@@ -1,16 +1,24 @@
 Uses Zcodegen, Delphi;
 
-var 
-  BuildDelphiXE3PackagesTask: tTask;
-  BuildDelphiXE4PackagesTask: tTask;
-
-
 procedure BuldDelphiPackages(aCompilerVersion: integer);
 begin
-  if Zcodegen('packages.zcproject', 'packages.zcconfig', Format('DELPHIVER="%s";LIBSUFFIX="%s"', [GetDelphiCompilerVersion(aCompilerVersion), GetDelphiPackageVersion(aCompilerVersion)]), '') <> 0 then
+  if Zcodegen('packages.zcproject', 'packages.zcconfig', Format('DELPHIVER="%s";LIBSUFFIX="%s"', [GetDelphiCompilerVersion(aCompilerVersion), GetDelphiPackageVersion(aCompilerVersion)]), wd, '') <> 0 then
      RaiseException(erCustomError, 'failed.');  
 end;
 
+procedure BuildDelphiXEPackages;
+begin
+  Output.log('Delphi XE Packages ...');
+
+  BuldDelphiPackages(DELPHIXE);
+end;  
+
+procedure BuildDelphiXE2Packages;
+begin
+  Output.log('Delphi XE2 Packages ...');
+
+  BuldDelphiPackages(DELPHIXE2);
+end;  
 
 procedure BuildDelphiXE3Packages;
 begin
@@ -26,24 +34,148 @@ begin
   BuldDelphiPackages(DELPHIXE4);
 end;  
   
+procedure BuildDelphiXE5Packages;
+begin
+  Output.log('Delphi XE5 Packages ...');
+
+  BuldDelphiPackages(DELPHIXE5);
+end;  
+
+procedure BuildDelphiXE6Packages;
+begin
+  Output.log('Delphi XE6 Packages ...');
+
+  BuldDelphiPackages(DELPHIXE6);
+end;
+
+procedure BuildDelphiXE7Packages;
+begin
+  Output.log('Delphi XE7 Packages ...');
+
+  BuldDelphiPackages(DELPHIXE7);
+end;
+
+procedure BuildDelphiXE8Packages;
+begin
+  Output.log('Delphi XE8 Packages ...');
+
+  BuldDelphiPackages(DELPHIXE8);
+end;
+
+procedure BuildDelphi10Packages;
+begin
+  Output.log('Delphi 10 Packages ...');
+
+  BuldDelphiPackages(DELPHI10);
+end;
+
+procedure BuildDelphi10_1Packages;
+begin
+  Output.log('Delphi 10.1 Packages ...');
+
+  BuldDelphiPackages(DELPHI10_1);
+end;
+
+procedure BuildDelphi10_2Packages;
+begin
+  Output.log('Delphi 10.2 Packages ...');
+
+  BuldDelphiPackages(DELPHI10_2);
+end;
+
 begin
   Output.log('Building Delphi Packages ...');
 
   Output.log('Working Directory: '+wd);
 
-  BuildDelphiXE3PackagesTask := Task.AddTask('BuildDelphiXE3Packages');
-  if Not Assigned(BuildDelphiXE3PackagesTask) then
-    RaiseException(erCustomError, 'not assigned BuildDelphiXE3PackagesTask'); 
-  BuildDelphiXE3PackagesTask.Criteria.Failed.Abort := True;
- 
-  
-  BuildDelphiXE4PackagesTask := Task.AddTask('BuildDelphiXE4Packages');
-  if Not Assigned(BuildDelphiXE4PackagesTask) then
-    RaiseException(erCustomError, 'not assigned BuildDelphiXE4PackagesTask'); 
-  BuildDelphiXE4PackagesTask.Criteria.Failed.Abort := True;
- 
+   with Task.AddTask('BuildDelphiXEPackages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;
 
-  if not Task.RunTargets(['BuildDelphiXE3Packages', 'BuildDelphiXE4Packages']) then 
+   with Task.AddTask('BuildDelphiXE2Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+
+   with Task.AddTask('BuildDelphiXE3Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;    
+
+   with Task.AddTask('BuildDelphiXE4Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+
+  with Task.AddTask('BuildDelphiXE5Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+ 
+  with Task.AddTask('BuildDelphiXE6Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+
+  with Task.AddTask('BuildDelphiXE7Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+
+   with Task.AddTask('BuildDelphiXE8Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+
+  with Task.AddTask('BuildDelphi10Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;  
+
+  with Task.AddTask('BuildDelphi10_1Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end; 
+
+   with Task.AddTask('BuildDelphi10_2Packages') do
+    begin
+      Criteria.Failed.Abort := True;
+     
+      
+    end;
+
+  if not Task.RunTargets(['BuildDelphiXEPackages', 
+        'BuildDelphiXE2Packages', 
+        'BuildDelphiXE3Packages', 
+        'BuildDelphiXE4Packages',
+        'BuildDelphiXE5Packages',
+        'BuildDelphiXE6Packages',
+        'BuildDelphiXE7Packages',
+        'BuildDelphiXE8Packages',
+        'BuildDelphi10Packages',
+        'BuildDelphi10_1Packages',
+        'BuildDelphi10_2Packages']) then 
     RaiseException(erCustomError, 'missing procedure.'); 
    
 
